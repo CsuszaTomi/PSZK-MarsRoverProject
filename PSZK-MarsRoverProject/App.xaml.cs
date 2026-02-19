aaa2f69fd23ed37b8cd5406cdcfa9c93e579d0a9
Dunka <pappd1@kkszki.hu>
2026-02-19 12:47:48 +0100

    Add project files.

---
+++ PSZK-MarsRoverProject/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace PSZK_MarsRoverProject
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
