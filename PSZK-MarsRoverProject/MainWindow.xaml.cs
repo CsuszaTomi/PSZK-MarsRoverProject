1e43f103ebdef0c7530a88975f73719c006261a9
HunTrooper <dgamer1118offical2@gmail.com>
2026-02-19 20:48:52 +0100

    Rover megjelenítése, "kamera" mozgás, roverre visszatérés "kamerával"

--- PSZK-MarsRoverProject/MainWindow.xaml.cs
+++ PSZK-MarsRoverProject/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using PSZK_MarsRoverProject.Models;
+using System.IO;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -19,30 +20,67 @@ namespace PSZK_MarsRoverProject
     public partial class MainWindow : Window
     {
         string[,] terkep = new string[50, 50];
+        Rover rover = new Rover() { Xposition = 32, Yposition = 34, BatteryLevel = 100, IsCharging = true };
         public MainWindow()
         {
             InitializeComponent();
             CsvBeolvaso();
+            terkep[32, 34] = "R";
+            JatekterFeltoltes();
+        }
+
+        private void JatekterFeltoltes()
+        {
             for (int i = 0; i < terkep.GetLength(0); i++)
             {
                 for (int j = 0; j < terkep.GetLength(1); j++)
                 {
                     TextBlock textBlock = new TextBlock()
                     {
-                        Height = 20,
-                        Width = 20,
+                        Height = 40,
+                        Width = 40,
                         Text = terkep[i, j],
                         TextAlignment = TextAlignment.Center,
                         VerticalAlignment = VerticalAlignment.Top,
                         HorizontalAlignment = HorizontalAlignment.Left,
-                        Foreground = terkep[i, j] == "#" ? Brushes.Red : Brushes.White,
-                        Margin = new Thickness(10 + j * 20,10 + i * 20, 0, 0)
+                        Foreground = terkep[i, j] == "#" ? Brushes.Red : terkep[i, j] == "R" ? Brushes.Green : Brushes.White,
+                        Margin = new Thickness(10 + j * 40, 10 + i * 40, 0, 0)
                     };
                     jatekter.Children.Add(textBlock);
                 }
             }
         }
-
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            double roverX = 34 * 40;
+            double roverY = 32 * 40;
+            double centerX = roverX - (kamera.ActualWidth / 2);
+            double centerY = roverY - (kamera.ActualHeight / 2);
+            kamera.ScrollToHorizontalOffset(centerX);
+            kamera.ScrollToVerticalOffset(centerY);
+        }
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.W:
+                    kamera.ScrollToVerticalOffset(kamera.VerticalOffset - 40);
+                    break;
+                case Key.S:
+                    kamera.ScrollToVerticalOffset(kamera.VerticalOffset + 40);
+                    break;
+                case Key.A:
+                    kamera.ScrollToHorizontalOffset(kamera.HorizontalOffset - 40);
+                    break;
+                case Key.D:
+                    kamera.ScrollToHorizontalOffset(kamera.HorizontalOffset + 40);
+                    break;
+                case Key.Space:
+                    kamera.ScrollToVerticalOffset(rover.Yposition * 40 - (kamera.ActualHeight / 2));
+                    kamera.ScrollToHorizontalOffset(rover.Xposition * 40 - (kamera.ActualWidth / 2));
+                    break;
+            }
+        }
         private void CsvBeolvaso()
         {
             if (!File.Exists("mars_map_50x50.csv"))
---
+++ PSZK-MarsRoverProject/Models/Rover.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSZK_MarsRoverProject.Models
+{
+    internal class Rover
+    {
+        public Rover() { }
+        public int Xposition { get; set; }
+        public int Yposition { get; set; }
+        public float BatteryLevel { get; set; }
+        public bool IsCharging { get; set; }
+
+    }
+}
