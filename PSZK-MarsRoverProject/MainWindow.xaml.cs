eaea526a6380c4b103cd257fddae9db1abbc88de
HunTrooper <dgamer1118offical2@gmail.com>
2026-02-20 18:05:54 +0100

    Képes generálásra váltás

--- PSZK-MarsRoverProject/MainWindow.xaml.cs
+++ PSZK-MarsRoverProject/MainWindow.xaml.cs
@@ -21,35 +21,76 @@ namespace PSZK_MarsRoverProject
     {
         string[,] terkep = new string[50, 50];
         Rover rover = new Rover() { Xposition = 32, Yposition = 34, BatteryLevel = 100, IsCharging = true };
+        Image roverKep;
         public MainWindow()
         {
             InitializeComponent();
             CsvBeolvaso();
-            terkep[32, 34] = "R";
+            //terkep[32, 34] = "R";
             JatekterFeltoltes();
         }
 
         private void JatekterFeltoltes()
         {
+            //tile rajzolás
             for (int i = 0; i < terkep.GetLength(0); i++)
             {
                 for (int j = 0; j < terkep.GetLength(1); j++)
                 {
-                    TextBlock textBlock = new TextBlock()
-                    {
-                        Height = 40,
-                        Width = 40,
-                        Text = terkep[i, j],
-                        TextAlignment = TextAlignment.Center,
-                        VerticalAlignment = VerticalAlignment.Top,
-                        HorizontalAlignment = HorizontalAlignment.Left,
-                        Foreground = terkep[i, j] == "#" ? Brushes.Red : terkep[i, j] == "R" ? Brushes.Green : Brushes.White,
-                        Margin = new Thickness(10 + j * 40, 10 + i * 40, 0, 0)
-                    };
-                    jatekter.Children.Add(textBlock);
+                    Image talaj = new Image() { Width = 40, Height = 40 };
+                    string jel = terkep[i, j] == "R" ? "." : terkep[i, j];
+                    talaj.Source = GetImageSource(jel);
+                    Canvas.SetLeft(talaj, j * 40);
+                    Canvas.SetTop(talaj, i * 40);
+                    jatekter.Children.Add(talaj);
                 }
             }
+            //rover rajzolás
+            roverKep = new Image()
+            {
+                Width = 40,
+                Height = 40,
+                Source = new BitmapImage(new Uri("pack://application:,,,/Images/rover.png")),
+            };
+            //A rover képe mindig a legfelső rétegben legyen
+            Panel.SetZIndex(roverKep, 10);
+            FrissitRoverPozicio();
+            jatekter.Children.Add(roverKep);
         }
+
+        /// <summary>
+        /// A rover pozíciójának frissítése a játéktéren, valamint a pozíció szövegének frissítése
+        /// </summary>
+        private void FrissitRoverPozicio()
+        {
+            Canvas.SetLeft(roverKep, rover.Xposition * 40);
+            Canvas.SetTop(roverKep, rover.Yposition * 40);
+            txtPos.Text = $"X: {rover.Xposition}, Y: {rover.Yposition}";
+        }
+
+        /// <summary>
+        /// A megadott karakter alapján visszaadja a megfelelő képet a játéktérhez
+        /// </summary>
+        /// <param name="karakter">A karakter, amelyhez a képet keresünk</param>
+        /// <returns>A megfelelő ImageSource</returns>
+        private ImageSource GetImageSource(string karakter)
+        {
+            string utvonal = "";
+            switch (karakter)
+            {
+                case ".":
+                    utvonal = "ground.png";
+                    break;
+                case "#":
+                    utvonal = "obstacle.png";
+                    break;
+                default:
+                    utvonal = "ground.png";
+                    break;
+            }
+            return new BitmapImage(new Uri($"pack://application:,,,/Images/{utvonal}"));
+        }
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             double roverX = 34 * 40;
@@ -79,7 +120,14 @@ namespace PSZK_MarsRoverProject
                     kamera.ScrollToVerticalOffset(rover.Yposition * 40 - (kamera.ActualHeight / 2));
                     kamera.ScrollToHorizontalOffset(rover.Xposition * 40 - (kamera.ActualWidth / 2));
                     break;
+                case Key.R:
+                    if (rover.Yposition > 0) rover.Yposition--;
+                    break;
+                case Key.T:
+                    if (rover.Yposition < 49) rover.Yposition++;
+                    break;
             }
+            FrissitRoverPozicio();
         }
         private void CsvBeolvaso()
         {
