8124757d3affd4cb91470967ab9071c17c71e3d9
Bálint <szilagyib1@kkszki.hu>
2026-03-05 12:50:32 +0100

    .

--- PSZK-MarsRoverProject/MainWindow.xaml.cs
+++ PSZK-MarsRoverProject/MainWindow.xaml.cs
@@ -239,5 +239,20 @@ namespace PSZK_MarsRoverProject
                 }
             }
         }
+
+        private void BoostButton3_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BoostButton2_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BoostButton1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
